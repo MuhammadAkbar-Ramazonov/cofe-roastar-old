02db8714230d5c27b525adaad893d6980754c45c
MuhammadAkbar-Ramazonov <ramazonov3600@gmail.com>
2022-08-12 05:25:03 +0500

    add mistakes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 var elOpenMenu = document.querySelector(".button")
 var elOpenList = document.querySelector(".sitenav")
-var elCloseBtn = document.querySelector(".button-close")
-var elOverly = document.querySelector(".overly")
 
 elOpenMenu.addEventListener("click", function(){
     elOpenList.classList.toggle("show-block");
-});
-elOpenMenu.addEventListener("click", function(){
-    elOpenMenu.classList.toggle("button-close");
-});
-elOpenMenu.addEventListener("click", function(){
-    elOverly.classList.toggle("overly-style");
 });
