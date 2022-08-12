f9baf7cd8c99ebe6e5388a13e693fc73580c557b
MuhammadAkbar-Ramazonov <ramazonov3600@gmail.com>
2022-08-12 13:29:42 +0500

    finish collection section

--- js/main.js
+++ js/main.js
@@ -3,4 +3,5 @@ var elOpenList = document.querySelector(".sitenav")
 
 elOpenMenu.addEventListener("click", function(){
     elOpenList.classList.toggle("show-block");
+    elOpenMenu.classList.toggle("button--active");
 });
