0a5731ee7c62413299f71c9ede936745e10412c6
MuhammadAkbar-Ramazonov <ramazonov3600@gmail.com>
2022-08-19 21:00:44 +0500

    add starter details section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-var elOpenMenu = document.querySelector(".button")
-var elOpenList = document.querySelector(".sitenav")
+var elOpenMenu = document.querySelector(".button");
+var elOpenList = document.querySelector(".sitenav");
+var elOpenModelBtn = document.querySelector(".site-inner-form-btn");
+var elOpenModel = document.querySelector(".modal");
+var elCloseModel = document.querySelector(".modal-btn");
 
 elOpenMenu.addEventListener("click", function(){
     elOpenList.classList.toggle("show-block");
     elOpenMenu.classList.toggle("button--active");
+});
+
+
+elOpenModelBtn.addEventListener("click", function(){
+    elOpenModel.classList.add("modal-open");
+});
+
+elCloseModel.addEventListener("click", function(){
+    elOpenModel.classList.remove("modal-open");
 });
